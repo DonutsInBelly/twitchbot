564c990a5159cdb446a8f61b3e97ce95ac36c83e
Carlin Au <carlinau@gmail.com>
2021-01-26 01:30:01 -0500

    feat(ShoutOut): added !so to shoutout other streamers in chat

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+import { ApiClient, UserIdResolvable } from "twitch";
 import { RefreshableAuthProvider, StaticAuthProvider } from "twitch-auth";
 import { ChatClient } from "twitch-chat-client";
 
@@ -15,14 +16,45 @@ async function main() {
       refreshToken,
     }
   );
+  const apiClient = new ApiClient({ authProvider: auth });
   const chatClient = new ChatClient(auth, { channels: ["dominusbelli"] });
   await chatClient.connect();
-  chatClient.onMessage((channel, user, message) => {
+  chatClient.onMessage(async (channel, user, message) => {
     if (message === "!ping") {
       chatClient.say(channel, "Pong!");
     } else if (message === "!dice") {
       const diceRoll = Math.floor(Math.random() * 6) + 1;
       chatClient.say(channel, `@${user} rolled a ${diceRoll}`);
+    } else if (message === "!discord") {
+      chatClient.say(
+        channel,
+        `@${user} here's a link to the Discord community! https://discord.gg/hhAUwzcuwT`
+      );
+    } else if (message.substring(0, 3).includes("!so")) {
+      let shoutout = message.slice(4);
+      const channelChatInfo = await apiClient.unsupported.getChatters(
+        "dominusbelli"
+      );
+      if (shoutout.startsWith("@")) {
+        shoutout = shoutout.slice(1);
+      }
+      try {
+        const shoutoutUserInfo = await apiClient.helix.users.getUserByName(
+          shoutout
+        );
+        const shoutoutChannelInfo = await apiClient.helix.channels.getChannelInfo(
+          shoutoutUserInfo?.id as UserIdResolvable
+        );
+        chatClient.say(
+          channel,
+          `Check out @${shoutout}! They were last seen playing ${shoutoutChannelInfo?.gameName}! https://www.twitch.tv/${shoutout}`
+        );
+      } catch (e) {
+        chatClient.say(
+          channel,
+          `Couldn't find a user with the name of ${shoutout}`
+        );
+      }
     }
   });
 }
